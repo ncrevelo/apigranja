5e5753241617c4df21d3a8ddc77ef03b2c3aa1ec
ncrevelo <111613787+ncrevelo@users.noreply.github.com>
2024-04-30 11:35:23 -0500

    Delete GranjaAzure-master directory

--- GranjaAzure-master/productos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import * 
-
-urlpatterns = [
-    path('v1/pro', Producto_APIView.as_view()),
-    path('v1/cat', CategoriaProducto_APIView.as_view()),
-    path('v1/cat/<int:pk>', CategoriaProducto_APIView_Detail.as_view()),
-]
